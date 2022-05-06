383b63dcc4858f301589bca90d21fc1bdb1d60be
Nikita Ziganshin <nikitaziganshin@outlook.com>
2022-05-06 16:11:17 +0300

    Forgot to edit Program.cs

--- TelegramApi/Program.cs
+++ TelegramApi/Program.cs
@@ -1,6 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace TelegramApi;
 
-app.MapGet("/", () => "Hello World!");
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
-app.Run();
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(builder =>
+            {
+                builder.UseStartup<Startup>();
+            });
+}
